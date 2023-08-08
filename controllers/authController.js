5c0fdace8b58715061f9a571b920d33b88095274
Anxhelo Cenollari <anxhelocenollari@gmail.com>
2023-08-08 17:42:05 +0200

    changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,7 +28,10 @@ module.exports = {
   loginUser: async (req, res) => {
     try {
       const user = await User.findOne({ email: req.body.email });
-      !user && res.status(401).json("Wrong credentials, provide a valid email");
+
+      if (!user) {
+        return res.status(401).json("Wrong credentials, provide a valid email");
+      }
 
       const decryptedPassword = CryptoJS.AES.decrypt(
         user.password,
@@ -36,8 +39,9 @@ module.exports = {
       );
       const decryptedpass = decryptedPassword.toString(CryptoJS.enc.Utf8);
 
-      decryptedpass !== req.body.password &&
-        res.status(401).json("Wrong password");
+      if (decryptedpass !== req.body.password) {
+        return res.status(401).json("Wrong password");
+      }
 
       const userToken = jwt.sign(
         {
